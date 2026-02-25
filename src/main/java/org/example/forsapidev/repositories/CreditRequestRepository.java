c3e5e06dd1607647afda28eaa164f5c46dd7c647
adem benali <benali.adem@esprit.tn>
2026-02-26 00:36:56 +0100

    1er phase backend (#1)
    Co-authored-by: bnadem39 <benali.adem39@esprit.tn>
    Co-authored-by: Oumayma M'rabet <Oumayma.M'RABET@esprit.tn>

---
+++ src/main/java/org/example/forsapidev/repositories/CreditRequestRepository.java
@@ -0,0 +1,10 @@
+package org.example.forsapidev.repositories;
+
+import org.example.forsapidev.entities.CreditManagement.CreditRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CreditRequestRepository extends JpaRepository<CreditRequest, Long> {
+}
+
