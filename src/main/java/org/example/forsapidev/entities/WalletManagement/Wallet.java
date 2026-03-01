86685c6f596a0bf482b0df5302b54f7e74a05903
Lina Sahbani <lina.sahbani04@gmail.com>
2026-03-01 22:09:18 +0100

    correction

--- src/main/java/org/example/forsapidev/entities/WalletManagement/Wallet.java
+++ src/main/java/org/example/forsapidev/entities/WalletManagement/Wallet.java
@@ -1,24 +1,55 @@
 package org.example.forsapidev.entities.WalletManagement;
 
+import org.example.forsapidev.entities.WalletManagement.Transaction;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Entity
-@Table(name = "wallet")
 public class Wallet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private WalletType type;
+    private Long ownerId;
 
     private BigDecimal balance;
 
-    private String currency;
+    @OneToMany(mappedBy = "wallet")
+    private List<Transaction> transactions;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastUpdated;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
+
